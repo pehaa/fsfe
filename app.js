87e7f7a49bc22d6b807e5b75f5d132642bfc3745
pehaa <info@pehaa.com>
2023-06-04 09:18:32 +0200

    change text

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require("http");
 
-http.createServer(function (req, res) {
- res.write("Pajka on the way to being a full stack engineer!");
- res.end();
-}).listen(3000);
+http
+	.createServer(function (req, res) {
+		res.write(
+			"Pajka on the way to being a full stack engineer! Close your eyes!"
+		);
+		res.end();
+	})
+	.listen(3000);
 
 console.log("Server started on port 3000");
-
